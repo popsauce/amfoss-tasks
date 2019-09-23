5329f123ea3075a2d9371ff35805f07574f07ca5
Karan <53134591+popsauce@users.noreply.github.com>
2019-09-23 22:43:53 +0530

    Update phase_six.py

--- task-4/phase_six.py
+++ task-4/phase_six.py
@@ -1,56 +1,82 @@
 import hashlib
 from ctypes import CDLL
 
-plainfirst=""
-plainsecond=""
-plainthird=""
-plainfourth=""
-plainfifth=""
 a="05181c2638270301601329023a203c2324072229230401273f2a1b0b02252b071e243e2313262923352411332336031b2f2d27191602383f07062e0827041725092e161e2d2f281017092f34123b321e2e12342d2b210c0b0e3f191f33370f1935000e2123392d132f163213152308302d0a0f3f282a0e183b"
 a=a.decode("hex")
-
-listone=[70,72,74,75,76,77,79,83,102,104,106,107,108,109,111,115]
-listwo=[65,70,71,74,75,76,77,81,84,85,86,87,97,102,103,106,107,108,109,110,114,115,117,119,120,121]
-listhree=[68,71,72,73,76,77,78,82,83,85,87,88,89,100,103,104,105,108,109,110,114,115,117,119,120,121]
-listfour=[65,67,70,71,72,74,75,76,80,81,82,86,87,97,99,102,103,104,106,107,108,112,113,114,118,119]
-listfive=[70,74,76,77,78,80,81,89,90,102,106,108,109,110,112,113,121,122]
-for aa in listone:
+o=0
+for aa in range(65,91) + range(97, 123):
+    if o==1:
+        break
+    plainfirst=""
     for i in range(0,125,5):
         p=ord(a[i])
         x=aa^p
         if x>=48 and x<=90 and x>=97 and x<=123:
-            plainfirst+=x
+            plainfirst+=ord(x)
         else:
             break
-    for ab in listwo:
+    for ab in range(65,91) + range(97, 123):
+        if o==1:
+            break
+        plainsecond=""
         for i in range(1,121,5):
             q=ord(a[i])
             x=ab^q
             if x>=48 and x<=90 and x>=97 and x<=123:
-                plainsecond+=x
+                plainsecond+=ord(x)
             else:
                 break    
-        for ac in listhree:
+        for ac in range(65,91) + range(97, 123):
+            if o==1:
+                break
+            plainthird=""
             for i in range(2,122,5):
                 r=ord(a[i])
                 x=ac^r
                 if x>=48 and x<=90 and x>=97 and x<=123:
-                    plainthird+=x
+                    plainthird+=ord(x)
                 else:
                     break
-            for ad in listfour:
+            for ad in range(65,91) + range(97, 123):
+                if o==1:
+                    break
+                plainfourth=""
                 for i in range(3,123,5):
                     s=ord(a[i])
                     x=ad^s
                     if x>=48 and x<=90 and x>=97 and x<=123:
-                        plainfourth+=x
+                        plainfourth+=ord(x)
                     else:
                         break
-                for ae in listfive:
+                for ae in range(65,91) + range(97, 123):
+                    if o==1:
+                        break
+                    plainfifth=""
                     for i in range(4,124,5):
                         t=ord(a[i])
                         x=ae^t
                         if x>=48 and x<=90 and x>=97 and x<=123:
-                            plainfifth+=x
+                            plainfifth+=ord(x)
                         else:
                             break
+                        plaintext=""
+                        p=chr(aa)
+                        q=chr(ab)
+                        r=chr(ac)
+                        s=chr(ad)
+                        t=chr(ae)
+                        k=p+q+r+s+t
+                        print k
+                        for zz in range(0,24):
+                            plaintext+=plainfirst[zz]+plainsecond[zz]+plainthird[zz]+plainfourth[zz]+plainfifth[zz]
+                        
+                        plaintext=plaintext+plainfirst[24]
+                        flag="{"+plaintext+"}"
+                        if hashlib.md5(flag).digest().encode("hex") == "d444d1ac3799a86d31f34b4a8a3243c6":
+                            o=1
+                            ans=flag
+                            kk=k                            
+print "----------------"
+print ans
+print "##########"
+print kk
