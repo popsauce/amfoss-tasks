76b7d120a32399c72e18a7b600f9b0bb34b34c1b
Karan <53134591+popsauce@users.noreply.github.com>
2019-09-28 20:51:25 +0530

    Update phase_six.py

--- task-4/phase_six.py
+++ task-4/phase_six.py
@@ -1,82 +1,102 @@
 import hashlib
 from ctypes import CDLL
 
-a="05181c2638270301601329023a203c2324072229230401273f2a1b0b02252b071e243e2313262923352411332336031b2f2d27191602383f07062e0827041725092e161e2d2f281017092f34123b321e2e12342d2b210c0b0e3f191f33370f1935000e2123392d132f163213152308302d0a0f3f282a0e183b"
-a=a.decode("hex")
-o=0
-for aa in range(65,91) + range(97, 123):
-    if o==1:
+cipone=[5, 39, 41, 35, 35, 42, 43, 35, 53, 54, 39, 63, 39, 46, 40, 52, 46, 33, 25, 25, 35, 22, 8, 63, 59]
+ciptwo=[24, 3, 2, 36, 4, 27, 7, 19, 36, 3, 25, 7, 4, 22, 16, 18, 18, 12, 31, 53, 57, 50, 48, 40]
+cipthree=[28, 1, 58, 7, 1, 11, 30, 38, 17, 27, 22, 6, 23, 30, 23, 59, 52, 11, 51, 0, 45, 19, 45, 42]
+cipfour=[38, 96, 32, 34, 39, 2, 36, 41, 51, 47, 2, 46, 37, 45, 9, 50, 45, 14, 55, 14, 19, 21, 10, 14]
+cipfive=[56, 19, 60, 41, 63, 37, 62, 35, 35, 45, 56, 8, 9, 47, 47, 30, 43, 63, 15, 33, 47, 35, 15, 24]
+br=0
+
+for aa in range(65,91)+ range(97,123):
+    if br==1:
         break
-    plainfirst=""
-    for i in range(0,125,5):
-        p=ord(a[i])
-        x=aa^p
-        if x>=48 and x<=90 and x>=97 and x<=123:
-            plainfirst+=ord(x)
+    loopone=0
+    plaone=""
+    for i in cipone:
+        x=aa^i
+        if x>=33 and x<=126:
+            plaone+=chr(x)
         else:
+            plaone=""
+            loopone=1
             break
-    for ab in range(65,91) + range(97, 123):
-        if o==1:
-            break
-        plainsecond=""
-        for i in range(1,121,5):
-            q=ord(a[i])
-            x=ab^q
-            if x>=48 and x<=90 and x>=97 and x<=123:
-                plainsecond+=ord(x)
-            else:
-                break    
-        for ac in range(65,91) + range(97, 123):
-            if o==1:
+    if loopone!=1:
+        for ab in range(65,91)+range(97,123):
+            if br==1:
                 break
-            plainthird=""
-            for i in range(2,122,5):
-                r=ord(a[i])
-                x=ac^r
-                if x>=48 and x<=90 and x>=97 and x<=123:
-                    plainthird+=ord(x)
+            looptwo=0
+            platwo=""
+            for j in ciptwo:
+                x=ab^j
+                if x>=33 and x<=126:
+                    platwo+=chr(x)
                 else:
+                    platwo=""
+                    looptwo=1
                     break
-            for ad in range(65,91) + range(97, 123):
-                if o==1:
-                    break
-                plainfourth=""
-                for i in range(3,123,5):
-                    s=ord(a[i])
-                    x=ad^s
-                    if x>=48 and x<=90 and x>=97 and x<=123:
-                        plainfourth+=ord(x)
-                    else:
+            if looptwo!=1:
+                for ac in range(65,91)+range(97,123):
+                    if br==1:
                         break
-                for ae in range(65,91) + range(97, 123):
-                    if o==1:
-                        break
-                    plainfifth=""
-                    for i in range(4,124,5):
-                        t=ord(a[i])
-                        x=ae^t
-                        if x>=48 and x<=90 and x>=97 and x<=123:
-                            plainfifth+=ord(x)
+                    loopthree=0
+                    plathree=""
+                    for k in cipthree:
+                        x=ac^k
+                        if x>=33 and x<=126:
+                            plathree+=chr(x)
                         else:
+                            plathree=""
+                            loopthree=1
                             break
-                        plaintext=""
-                        p=chr(aa)
-                        q=chr(ab)
-                        r=chr(ac)
-                        s=chr(ad)
-                        t=chr(ae)
-                        k=p+q+r+s+t
-                        print k
-                        for zz in range(0,24):
-                            plaintext+=plainfirst[zz]+plainsecond[zz]+plainthird[zz]+plainfourth[zz]+plainfifth[zz]
-                        
-                        plaintext=plaintext+plainfirst[24]
-                        flag="{"+plaintext+"}"
-                        if hashlib.md5(flag).digest().encode("hex") == "d444d1ac3799a86d31f34b4a8a3243c6":
-                            o=1
-                            ans=flag
-                            kk=k                            
-print "----------------"
+                    if loopthree!=1:
+                        for ad in range(65,91)+range(97,123):
+                            if br==1:
+                                break
+                            loopfour=0
+                            plafour=""
+                            for l in cipfour:
+                                x=ad^l
+                                if x>=33 and x<=126:
+                                    plafour+=chr(x)
+                                else:
+                                    plafour=""
+                                    loopfour=1
+                                    break
+                            if loopfour!=1:
+                                for ae in range(65,91)+range(97,123):
+                                    if br==1:
+                                        break
+                                    loopfive=0
+                                    plafive=""
+                                    for m in cipfive:
+                                        x=ae^m
+                                        if x>=33 and x<=126:
+                                            plafive+=chr(x)
+                                        else:
+                                            plafive=""
+                                            loopfive=1
+                                            break
+                                    if loopfive!=1:
+                                        plaintext=""
+                                        aaa=chr(aa)
+                                        aab=chr(ab)
+                                        aac=chr(ac)
+                                        aad=chr(ad)
+                                        aae=chr(ae)
+                                        value=aaa+aab+aac+aad+aae
+                                        print value
+                                        for a in range(0,24):
+                                            plaintext+=plaone[a]+platwo[a]+plathree[a]+plafour[a]+plafive[a]
+                                        plaintext=plaintext+plaone[24]
+                                        flag="{"+plaintext+"}"
+                                        if hashlib.md5(flag).digest().encode("hex") == "d444d1ac3799a86d31f34b4a8a3243c6":
+                                            print "!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!"
+                                            print "Yeah!....You are a genius!"
+                                            print ""
+                                            ans=value
+                                            fin=flag
+                                            br=1
 print ans
-print "##########"
-print kk
+print ""
+print fin
